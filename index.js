50762b9111e37d80d34f69b90c39a5bda4a1be8d
Caduella <146942933+Caduella@users.noreply.github.com>
2024-01-21 22:02:21 +0100

    Mongoose models

--- index.js
+++ index.js
@@ -1,170 +1,184 @@
 const express = require('express'),
 			app = express(),
-			morgan = require('morgan'),
-			bodyParser = require('body-parser'),
+			morgan = require('morgan'),			
 			uuid = require('uuid');
+			
+const mongoose = require('mongoose');
+const Models = require ('./models.js');
+const Movies = Models.Movie;
+const Users = Models.User;
 		
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+//Log requests to server
 app.use(morgan('common'));
 
-let users = [
-	{
-		id: 1,
-		name: 'Kim',
-		favoriteMovies: []
-	},
-	{
-		id: 2,
-		name: 'Joe',
-		favoriteMovies: ['The Fountain']
-	}
-];
+mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-let movies = [
-	{
-		"Title": "The Fountain",
-		"Description":"As a modern-day scientist, Tommy is strugging with mortality, desperately searching for the medical breakthrough that will save the life of his cancer-stricken wife, Izzi.",
-		"Genre": {
-			"Name":"Drama",
-			"Description":"In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-		},
-		"Director": {
-			"Name":"Darren Aronofsky",
-			"Bio":"Darren Aronofsky was born February 12, 1969, in Brooklyn, New York.  Growing up, Darren was always artistic: he loved classic movies and, as a teenage, he even spent time doing graffiti art.  After high school, Darren went to Harvard University to study film (both live-action and animation).  He won several film awards after completing his senior thesis film, Supermarket Sweep, starring Sean Gullette, which went on to becoming a National Student Academy Award finalist.  Aronofsky didn't make a feature film until five years later, in Februray 1996, where he began creating the concept for Pi (1998).  After Darren's script for Pi (1998) received great reaction from friends, he began production.  The film re-teamed Aronofsky with Gullette, who played the lead.  This went on to further successes, such as Requiem for a Dream (2000), The Wrestler (2008) and Black Swan (2010).  Most recently, he completed the films Noah (2014) and Mother! (2017).",
-			"Birth":1969.0
-		},
-		"ImageURL":"https://m.media-amazon.com/images/M/MV5BMTU5OTczMTcxMV5BMl5BanBnXkFtZTcwNDg3MTEzMw@@._V1_FMjpg_UX1000_.jpg",
-		"Featured":false
-	},
-	{
-		"Title":"The Princess Bride",
-		"Description":"While home sick in bed, a young boy's grandfather reads him the story of a farmboy-turned-pirate who encounters numerous obstacles, enemies and allies in his quest to be reunited with his true love.",
-		"Genre":{
-			"Name":"Action",
-			"Description":"Action film is a film genre in which the protagonist or protagonists are thrust into a series of events that typically include violence, extended fighting, physical feats, rescues and frantic chases.  Action films tend to feature a mostly resourceful hero struggling against incredible odds, which included life-threatening situations, a dangerous villain, or a pursuit which usually concludes in victory for the hero."
-		},
-		"Director": {
-			"Name":"Rob Reiner",
-			"Bio":"When Rob graduated high school, his parents advised him to participate in Summer Theatre.  Reiner got a job as an apprentice in the Bucks County Playhouse in Pennsylvania.  He went to be further educated at UCLA Film School.  Reiner felt he still wasn't successful even having a recurring role on one of the biggest shows in the country, All in the Family.  Reiner began his directing career with the Oscar-nominated films This is Spinal Tap, Stand By Me, and the Princess Bride.",
-			"Birth":1947.0
-		},
-		"ImageURL":"https://image.tmdb.org/t/p/original/A09tSLrBBspWHlc2tOBaZNfdlbF.jpg",
-		"Featured":false
-	},
-	{}
-];
+//Default text response when at /
+app.get('/', (req, res) =>{
+	res.send('Welcome to MyQuickMovie API');
+});
 
 //Return a list of ALL movies to the user
-app.get('/movies', (req, res) => {
-	res.status(200).json(movies)
+app.get('/movies', async (req, res) => {
+  await Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+//Return a list of all users
+app.get('/users', async (req, res) => {
+	await Users.find()
+	.then((users) => {
+		res.status(201).json(users);
+	})
+	.catch((err) => {
+		console.error(err);
+		res.status(500).send('Error: ' + err);
+	});
 })
+
 
 //Return data about a single movie by title
-app.get('/movies/:title', (req, res) => {
-	const { title } = req.params;
-	const movie = movies.find( movie => movie.Title === title );
+app.get('/movies/:Title', async (req, res) => {
+  await Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-	if (movie) {
-		res.status(200).json(movie);
-	} else {
-		res.status(400).send('no such movie')
-	}
-})
+//Return movies by its genre 
+app.get('/movies/genres/:genreName', async (req, res) => {
+  await Movies.find({ 'Genre.Name': req.params.genreName })
+    .then((movies) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-//Return data about a genre by name
-app.get('/movies/genre/:genreName', (req, res) => {
-	const { genreName } = req.params;
-	const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
 
-	if (genre) {
-		res.status(200).json(genre);
-	} else {
-		res.status(400).send('no such genre')
-	}
-})
+//Return a movie by a given director's name 
+app.get('/movies/directors/:directorName', async (req, res) => {
+  await Movies.findOne({ 'Director.Name': req.params.directorName })
+    .then((movies) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-//Return data about a director
-app.get('/movies/directors/:directorName', (req, res) => {
-	const { directorName } = req.params;
-	const director = movies.find( movie => movie.Director.Name === directorName ).Director;
-
-	if (director) {
-		res.status(200).json(director);
-	} else {
-		res.status(400).send('no such director')
-	}
-})
-
-//Allow new users to register
-app.post('/users', (req, res) => {
-	const newUser = req.body;
-
-	if (newUser.name) {
-		newUser.id = uuid.v4();
-		users.push(newUser);
-		res.status(201).json(newUser)
-	} else {
-		res.status(400).send('users need names')
-	}
-})
+//Add a new user
+app.post('/users', async (req, res) => {
+  await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday,
+				})
+          .then((user) =>{res.status(201).json(user) })  
+					.catch((error) => {
+						console.error(error);
+						res.status(500).send('Error: ' + error);
+					})      	
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 //Allow users to update their user info (username)
-app.put('/users/:id', (req, res) => {
-	const { id } = req.params;
-	const updatedUser = req.body;
+app.put('/users/:Username', async (req, res) => {
+  await Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  })
 
-	let user = users.find( user => user.id == id);
-
-	if (user) {
-			user.name = updatedUser.name;
-			res.status(200).json(user);
-	} else {
-			res.status(400).send('no such user')
-	}
-})
+});
 
 //Allow users to add a movie to their list of favorites
-app.patch('/users/:id/favoriteMovies/:movieTitle', (req, res) => {
-	const { id, movieTitle } = req.params;
-	
-	let user = users.find( user => user.id == id);
+app.post('/users/:Username/Movies/:MovieID', async (req, res) => {
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
 
-	if (user) {
-			user.favoriteMovies.push(movieTitle);
-			res.status(200).send(`${movieTitle} has been successfully added to user ${id}'s list of favorite movies.`);
-	} else {
-			res.status(400).send('no such user')
-	}
-})
 
 //Allow users to remove a movie from their list of favorites
-app.delete('/users/:id/favoriteMovies/:movieTitle', (req, res) => {
-	const { id, movieTitle } = req.params;
-	
-	let user = users.find( user => user.id == id);
+app.delete('/users/:Username/Movies/:MovieID', async (req, res) => {
+	await Users.findOneAndUpdate({ Username: req.params.Username },	{ 
+		$pull: { FavoriteMovies: req.params.MovieID } 
+	},
+	{ new: true }) // This line makes sure that the updated document is returned
+	.then ((updatedUser) => {
+    res.json(updatedUser);
+  })
+	.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
+});
 
-	if (user) {
-			user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
-			res.status(200).send(`${movieTitle} has been successfully removed from user ${id}'s list of favorite movies.`);
-	} else {
-			res.status(400).send('no such user')
-	}
-})
 
 //Allow existing users to deregister
-app.delete('/users/:id', (req, res) => {
-	const { id } = req.params;
-	
-	let user = users.find( user => user.id == id);
-
-	if (user) {
-			users = users.filter( user => user.id != id);
-			res.status(200).send(`user ${id} has been successfully deleted.`);
-	} else {
-			res.status(400).send('no such user')
-	}
-})
+app.delete('/users/:Username', async (req, res) => {
+  await Users.findOneAndDelete({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 
 app.listen(8080, () => console.log('Listening on port 8080.'));
